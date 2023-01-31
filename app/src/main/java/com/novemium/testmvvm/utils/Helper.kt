507cfc1feed16eb8b8d6b4e6361c129988d0f3ea
dahoumeziane <hd_meziane@esi.dz>
2023-01-31 15:36:30 +0100

    first commit

---
+++ app/src/main/java/com/novemium/testmvvm/utils/Helper.kt
@@ -0,0 +1,9 @@
+package com.novemium.testmvvm.utils
+
+
+
+object Helper {
+    var apiUrl = "https://f9af-105-235-135-72.eu.ngrok.io/api/"
+
+
+}
---
+++ app/src/main/java/com/novemium/testmvvm/utils/RetrofitInstance.kt
@@ -0,0 +1,22 @@
+package com.novemium.testmvvm.utils
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object RetrofitInstance {
+    val client = OkHttpClient.Builder()
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .writeTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(60, TimeUnit.SECONDS)
+        .build()
+
+    fun instance(baseUrl: String): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+}
